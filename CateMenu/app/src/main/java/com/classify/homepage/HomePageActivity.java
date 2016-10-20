8386a6487326c29e9d108db35116c8c0b1247328
hkl778250693 <778250693@qq.com>
2016-10-20 23:48:30 +0800

    no message

---
+++ CateMenu/app/src/main/java/com/classify/homepage/HomePageActivity.java
@@ -0,0 +1,17 @@
+package com.classify.homepage;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.administrator.catemenu.R;
+
+/**
+ * Created by Administrator on 2016/10/20.
+ */
+public class HomePageActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_homepage_classify);
+    }
+}

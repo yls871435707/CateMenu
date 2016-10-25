78b36d8882056a894077c01578e2c58671d3f733
Your Name <you@example.com>
2016-10-25 23:23:16 +0800

    新建Activity

---
+++ CateMenu/app/src/main/java/com/navigationpage/GuidePageTwo.java
@@ -0,0 +1,16 @@
+package com.navigationpage;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.administrator.catemenu.R;
+
+/**
+ * Created by Administrator on 2016/10/25.
+ */
+public class GuidePageTwo extends Activity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_guidepage_tow);
+    }
+}

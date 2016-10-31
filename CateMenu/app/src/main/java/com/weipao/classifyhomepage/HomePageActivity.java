682764ed20d6273e2369d44e79b008f1a259b87d
Your Name <you@example.com>
2016-10-31 22:07:37 +0800

    首页页面

--- CateMenu/app/src/main/java/com/weipao/classifyhomepage/HomePageActivity.java
+++ CateMenu/app/src/main/java/com/weipao/classifyhomepage/HomePageActivity.java
@@ -12,6 +12,6 @@ public class HomePageActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_homepage_classify);
+        setContentView(R.layout.fragment_homepage);
     }
 }

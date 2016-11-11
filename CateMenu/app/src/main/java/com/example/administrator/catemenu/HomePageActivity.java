d6ce125033cee2fd289a3ceac41f23f28c07be27
Your Name <you@example.com>
2016-11-11 22:46:05 +0800

    fragment的添加和商城页面的一半

--- CateMenu/app/src/main/java/com/example/administrator/catemenu/HomePageActivity.java
+++ CateMenu/app/src/main/java/com/example/administrator/catemenu/HomePageActivity.java
@@ -1,15 +1,93 @@
 package com.example.administrator.catemenu;
 
 import android.app.Activity;
+
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+
+import com.fragment.ClassifyFragment;
+import com.fragment.HomepageFragment;
+import com.fragment.ShopFragment;
 
 /**
  * Created by Administrator on 2016/10/20.
  */
 public class HomePageActivity extends Activity {
+    HomepageFragment homepageFragment;
+    ClassifyFragment classifyFragment;
+    ShopFragment shopFragment;
+    LinearLayout linearLayout;
+    RadioButton homepagerb;
+    RadioButton classifyrb;
+    RadioButton shoprb;
+    RadioButton feastrb;
+    RadioButton squarerb;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
+        linearLayout = (LinearLayout) findViewById(R.id.ll);
+        homepagerb = (RadioButton) findViewById(R.id.rb_homepage);
+        classifyrb = (RadioButton) findViewById(R.id.rb_classify);
+        shoprb = (RadioButton) findViewById(R.id.rb_shop);
+        feastrb = (RadioButton) findViewById(R.id.rb_feast);
+        squarerb = (RadioButton) findViewById(R.id.rb_square);
+        homepagerb.setOnClickListener(clickListener);
+        classifyrb.setOnClickListener(clickListener);
+        shoprb.setOnClickListener(clickListener);
+        feastrb.setOnClickListener(clickListener);
+        squarerb.setOnClickListener(clickListener);
+
+        homepagerb.setChecked(true);
+        if (homepagerb.isChecked()) {
+            addHomepageFragment();
+        }
+    }
+
+    View.OnClickListener clickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            FragmentManager fragmentManager = getFragmentManager();
+            FragmentTransaction transaction = fragmentManager.beginTransaction();
+            switch (v.getId()) {
+                case R.id.rb_homepage:
+                    if (homepageFragment == null) {
+                        homepageFragment = new HomepageFragment();
+                    }
+                    transaction.replace(R.id.ll, homepageFragment);
+                    break;
+                case R.id.rb_classify:
+                    if (classifyFragment == null) {
+                        classifyFragment = new ClassifyFragment();
+                    }
+                    transaction.replace(R.id.ll, classifyFragment);
+                    break;
+                case R.id.rb_shop:
+                    if (shopFragment==null){
+                        shopFragment=new ShopFragment();
+                    }
+                    transaction.replace(R.id.ll,shopFragment);
+                    break;
+                case R.id.rb_feast:
+                    break;
+                case R.id.rb_square:
+                    break;
+            }
+            transaction.commit();
+        }
+    };
+
+    public void addHomepageFragment() {
+        homepageFragment = new HomepageFragment();
+        FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.ll, homepageFragment);
+        fragmentTransaction.commit();
     }
 }
---
+++ CateMenu/app/src/main/java/com/fragment/ShopFragment.java
@@ -0,0 +1,22 @@
+package com.fragment;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.administrator.catemenu.R;
+
+/**
+ * Created by Administrator on 2016/11/11.
+ */
+public class ShopFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view= inflater.inflate(R.layout.fragment_shop,null);
+        return view;
+    }
+}

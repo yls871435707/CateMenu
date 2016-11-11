7f53581a6fc8d701a4459f8f75319c246c7d2d70
黄奎良\abc <778250693@qq.com>
2016-11-11 16:54:09 +0800

    修改结构

--- CateMenu/app/src/main/java/com/example/administrator/catemenu/ShareActivity.java
+++ CateMenu/app/src/main/java/com/example/administrator/catemenu/ShareActivity.java
@@ -4,12 +4,12 @@ import android.app.Activity;
 import android.os.Bundle;
 
 /**
- * Created by Administrator on 2016/11/9.
+ * Created by abc on 2016/11/11.
  */
-public class MenuFileActivity extends Activity {
+public class ShareActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_cate_clip);
+        setContentView(R.layout.activity_share);
     }
 }

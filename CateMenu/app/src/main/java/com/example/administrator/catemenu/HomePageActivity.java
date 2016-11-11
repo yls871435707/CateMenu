aa8bbf1dc229c1711255c029cd0f929fff474814
yanliangshu <472896167@qq.com>
2016-11-11 17:40:31 +0800

    修改错误

--- CateMenu/app/src/main/java/com/example/administrator/catemenu/HomePageActivity.java
+++ CateMenu/app/src/main/java/com/example/administrator/catemenu/HomePageActivity.java
@@ -1,21 +1,15 @@
 package com.example.administrator.catemenu;
 
 import android.app.Activity;
-import android.app.Fragment;
 import android.os.Bundle;
-import android.view.View;
 
 /**
  * Created by Administrator on 2016/10/20.
  */
 public class HomePageActivity extends Activity {
-    View fragment;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
-        fragment = findViewById(R.id.homepage_fragment);
-
     }
 }

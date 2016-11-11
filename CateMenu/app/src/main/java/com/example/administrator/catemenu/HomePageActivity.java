d0a533c8486c3b469b041244adf309d4be5b6965
Your Name <you@example.com>
2016-11-11 14:16:28 +0800

    修改主页结构

--- CateMenu/app/src/main/java/com/example/administrator/catemenu/HomePageActivity.java
+++ CateMenu/app/src/main/java/com/example/administrator/catemenu/HomePageActivity.java
@@ -11,5 +11,6 @@ public class HomePageActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
+
     }
 }
